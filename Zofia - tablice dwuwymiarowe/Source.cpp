418d4999902ebcef471f49e62897e7584a400106
skylerr091 <skylerr091@gmail.com>
2024-12-07 17:19:58 +0100

    asdasd

--- Zofia - tablice dwuwymiarowe/Source.cpp
+++ Zofia - tablice dwuwymiarowe/Source.cpp
@@ -4,191 +4,93 @@ using namespace std;
 
 //const int MAX_W = 3, MAX_K = 4;
 
-const int MAX_W = 2, MAX_K = 2;
+const int MAX_W = 1001, MAX_K = 1001;
 
-int tab[MAX_W][MAX_K]; // tablica zostala automatycznie wyzerowana - tablica globalna
+char tab[MAX_W][MAX_K]; // tablica zostala automatycznie wyzerowana -> '\0'
 
-void wczytaj()
+void wyzeruj(int w, int k) // wstaw spacje
 {
-	cout << "Podaj elementy tablicy: " << endl;
-
-	for (int i = 0; i < MAX_W; i++)
+	for (int i = 0; i < w; i++)
 	{
-		for (int j = 0; j < MAX_K; j++)
-			cin >> tab[i][j];
+		for (int j = 0; j < k; j++)
+			tab[i][j] = ' ';
 	}
-
-	/*for (int j = 0; j < MAX_K; j++)
-	{
-		for (int i = 0; i < MAX_W; i++)
-			cin >> tab[i][j];
-	}*/
 }
 
-void wypisz()
+void wypisz(int w, int k)
 {
 	cout << "Wyswietlam elementy tablicy: " << endl;
 
-	for (int i = 0; i < MAX_W; i++)
+	for (int i = 0; i < w; i++)
 	{
-		for (int j = 0; j < MAX_K; j++)
-			cout << tab[i][j] << " ";
+		for (int j = 0; j < k; j++)
+			cout << tab[i][j];
 		cout << endl;
 	}
 }
 
-void iloczyn()
+void boki_k(int w, int k)
 {
-	int pom = 1;
+	for (int j = 0; j < k; ++j)
+		tab[0][j] = '*';
 
-	// 5 % 2 -> 1
-	// 5 % 3 -> 2
+	for (int j = 0; j < k; ++j)
+		tab[w-1][j] = '*';
 
-	for (int i = 0; i < MAX_W; i++)
-	{
-		for (int j = 0; j < MAX_K; j++)
-			if (tab[i][j] % 7 != 0) 
-			{
-				pom *= tab[i][j];
-				//pom = pom * tab[i][j];
-			}
+	for (int i = 0; i < w; ++i)
+		tab[i][0] = '*';
 
-	}
-
-	cout << "Iloczyn liczb niepodzielnych prezez 7 = " << pom << endl;
+	for (int i = 0; i < w; ++i)
+		tab[i][k-1] = '*';
 }
 
-void ilosc()
+void przekatna_1(int w, int k)
 {
-	int pom2 = 0;
+	int start_kolumna = 1;
 
-	for (int i = 0; i < MAX_W; i++)
+	for (int i = w - 2; i >= 1; i--) // petla glowna - leci po wierszach
 	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (i * j != 0)
-				pom2++;
-		}
+		tab[i][start_kolumna] = '#';
 
+		start_kolumna++;
 	}
-
-	cout << "Ilosc elementow ktorych iloczyn ideksow jest dodatni: " << pom2 << endl;
 }
-
-void sprawdzenie4()
+void przekatna_2(int w, int k)
 {
-	int pom3=0;
-	
-	for (int i = 0; i < MAX_W; i++)
+	int start_wiersz = 1;
+
+	for (int j = 1; j < k - 1; j++)
 	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (tab[i][j] > 4)
-				pom3++;
-		}
-
+		tab[start_wiersz][j] = 'O';
+		start_wiersz++;
 	}
-	if (pom3 == 0)
-		cout << "Nie ma liczby wiekszej od 4\n";
-	else
-		cout << "Jest liczba wieksza od 4\n";
-}
-
-void sprawdzenie4_v2()
-{
-	int pom3 = 0;
-
-	for (int i = 0; i < MAX_W; i++)
-	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (tab[i][j] > 4)
-			{
-				cout << "Jest liczba wieksza od 4\n";
-				return;
-			}
-		}
-
-	}
-	cout << "Nie ma liczby wiekszej od 4\n";
-}
-
-void ilossuma_nieparxystych()
-{
-	int pom2 = 0;
-	bool jest_w_nieparzystymW = false;
-
-	for (int i = 0; i < MAX_W; i++)
-	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (i % 2 == 1) {
-				pom2 += tab[i][j];
-				jest_w_nieparzystymW = true;
-			}
-		}
-
-	}
-
-	if (jest_w_nieparzystymW)
-	{
-		cout << "Suma elementow z wierszy nieparzystych:  " << pom2 << endl;
-	}
-	else
-		cout << "Suma z wierszy nieparzystych nie istnieje" << endl;
-	//cout << "Suma elementow z wierszy nieparzystych:  " << pom2 << endl;
 
 }
 
-void zmniejszanie()
+void przekatna_2_v2(int w, int k)
 {
-	for (int i = 0; i < MAX_W; i++)
+	for (int j = 1; j < k - 1; j++)
 	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (tab[i][j]<0)
-			{
-				tab[i][j] -= 2;
-			}
-		}
-
+		tab[j][j] = 'O';
 	}
-
-	wypisz();
-}
-
-void sprawdzenie10()
-{
-	int pom3 = 0;
-
-	for (int i = 0; i < MAX_W; i++)
-	{
-		for (int j = 0; j < MAX_K; j++)
-		{
-			if (tab[i][j] >10)
-			{
-				cout << "Nie wszystkie liczby sa mniejsze od 10\n";
-				return;
-			}
-		}
-
-	}
-	cout << "Wszystkie liczby sa mniejsze od 10\n";
 }
 
 int main()
 {
-	wczytaj();
-	wypisz();
+	int n;
 
-	iloczyn();
-	ilosc();
-	sprawdzenie4();
-	sprawdzenie4_v2();
-	ilossuma_nieparxystych();
-	zmniejszanie();
+	cout << "Podaj dlugosc boku kwadratu: ";
 
-	sprawdzenie10();
+	cin >> n;
+
+	wyzeruj(n, n);
+
+	boki_k(n, n);
+	przekatna_1(n, n);
+	przekatna_2(n, n);
+
+
+	wypisz(n, n);
 
 	return 0;
 }
