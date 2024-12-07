cb82342de14c56e6ae5ba7659cfa5354ba18f390
skylerr091 <skylerr091@gmail.com>
2024-12-07 17:33:32 +0100

    dsfsfd

--- Zofia - tablice dwuwymiarowe/Source.cpp
+++ Zofia - tablice dwuwymiarowe/Source.cpp
@@ -7,13 +7,14 @@ using namespace std;
 const int MAX_W = 1001, MAX_K = 1001;
 
 char tab[MAX_W][MAX_K]; // tablica zostala automatycznie wyzerowana -> '\0'
+bool tab_pom[MAX_W][MAX_K];
 
 void wyzeruj(int w, int k) // wstaw spacje
 {
 	for (int i = 0; i < w; i++)
 	{
 		for (int j = 0; j < k; j++)
-			tab[i][j] = ' ';
+			tab_pom[i][j] = true;
 	}
 }
 
@@ -72,6 +73,62 @@ void przekatna_2_v2(int w, int k)
 	for (int j = 1; j < k - 1; j++)
 	{
 		tab[j][j] = 'O';
+	}
+}
+
+bool sprawdz_czyX(int i, int j)
+{
+	return tab[i][j] == 'X';
+}
+
+bool sprawdz_czyY(int i, int j)
+{
+	return tab[i][j] == 'Y';
+}
+
+bool sprawdz_kierunek_prawo_dol(int start_i, int start_j, char c)
+{
+
+}
+
+bool sprawdz_kierunek_lewo_dol(int start_i, int start_j, char c)
+{
+
+}
+
+void policz_prawo_dol(int start_i, int start_j, char c)
+{
+
+}
+void policz_lewo_dol(int start_i, int start_j, char c)
+{
+
+}
+
+void program(int w, int k)
+{
+	for (int i = 0; i < w; i++) // g³wówny program
+	{
+		for (int j = 0; j < k; ++j)
+		{
+
+			if (tab_pom[i][j] == true) {
+				if (sprawdz_czyX(i, j))
+				{
+					if (sprawdz_kierunek_lewo_dol(i, j, 'X'))
+						policz_lewo_dol(i, j, 'X');
+					else
+						policz_prawo_dol(i, j, 'X');
+				}
+				else if (sprawdz_czyY(i, j))
+				{
+					if (sprawdz_kierunek_lewo_dol(i, j, 'Y'))
+						policz_lewo_dol(i, j, 'Y');
+					else
+						policz_prawo_dol(i, j, 'Y');
+				}
+			}
+		}
 	}
 }
 
